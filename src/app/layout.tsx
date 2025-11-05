452425aad4768d26494800c90e3f752baa8935c0
Iris-Ghislaine <ganzairis03@gmail.com>
2025-11-05 16:29:31 +0200

    created a home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import HomePage from "./home/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
